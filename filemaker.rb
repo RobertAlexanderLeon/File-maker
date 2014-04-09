6e612543df176085daf8e765dca438002bfda567
Robert Leon <llexi@hotmail.com>
2014-04-09 13:07:17 +0100

    created FilemakerEXTREME using symbols to name arguments and lambda default values

--- filemaker.rb
+++ filemaker.rb
@@ -1,2 +1,26 @@
-file = File.open("Hello_from_ruby.text", "w")
+require "color_text"
+puts "HEY EVERYBODY".purple
+puts "Hi Dr.Nick!".green
+puts "Today we're going to store your input for future use!".purple
+puts "Please press enter to continue".neon
+
+input = gets.chomp
+
+puts "Type:
+'save' - to save input to file
+'read' - to read the current file
+'source' - view the source code in the terminal"
+command = gets.chomp
+
+def read_file(named: name, using: -> (content) {content.read})
+	using.call(File.open(named))
+end
+
+case command
+	 when "save" then File.open("Hello_from_ruby.text", "w"){ |content| content.write input}
+     when "read" then puts read_file("Hello_from_ruby.text")
+     when "source" then puts read_file(named: "filemaker.rb", using: -> (content) {content.read})
+end
+
+# file = File.open("Hello_from_ruby.text", "w")
 #This creates a new file called 'string.txt'
---
+++ filemakerEXTREME.rb
@@ -0,0 +1,38 @@
+require "color_text"
+puts "HEY EVERYBODY".purple
+puts "Hi Dr.Nick!".green
+puts "Today we're going to store your input for future use!".purple
+puts "Please enter your text input or just press enter to continue".neon
+
+@input = gets.chomp
+
+puts "Type:
+'add' - to add input to file
+'save' - to save input to file
+'read' - to read the current file
+'source' - view the source code in the terminal"
+
+command = gets.chomp
+
+def read_file(named: name, using: -> (content) {content.read})
+	using.call(File.open(named))
+end
+
+def write_to(file: name, write: -> (content) {content.write @input})
+	write.call(File.open(master, "w"))
+end
+
+def add_to(document: name, add: -> (content) {content.write @input})
+	add.call(File.open(document, "a"))
+end
+
+
+case command
+     when "add" then add_to document: "Hello_from_ruby.txt" 
+	 when "save" then write_to file: "Hello_from_ruby.txt"	
+     when "read" then puts read_file named: "Hello_from_ruby.txt"
+     when "source" then puts read_file named: "filemaker.rb" 
+end
+
+# file = File.open("Hello_from_ruby.text", "w")
+#This creates a new file called 'string.txt'
